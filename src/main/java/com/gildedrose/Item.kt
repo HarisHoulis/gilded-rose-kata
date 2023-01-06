5c7578bc25c8b59826ce8b20182ce210032b50b8
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-01-06 16:49:31 +0100

    Reorder methods and merge update1 and update3

--- src/main/java/com/gildedrose/Item.kt
+++ src/main/java/com/gildedrose/Item.kt
@@ -15,72 +15,61 @@ open class BaseItem(
 ) : Item(name, sellIn, quality) {
 
     fun update() {
-        update1()
         age()
-        update3()
-    }
-
-    protected open fun update1() {
-        if (quality > 0) {
-            quality = quality - 1
-        }
+        degrade()
     }
 
     protected open fun age() {
-        sellIn = sellIn - 1
+        sellIn -= 1
     }
 
-    protected open fun update3() {
+    protected open fun degrade() {
+        if (quality > 0) {
+            quality -= 1
+        }
         if (sellIn < 0) {
             if (quality > 0) {
-                quality = quality - 1
+                quality -= 1
             }
         }
     }
 }
 
 class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun update1() {
+    override fun degrade() {
         if (quality < 50) {
-            quality = quality + 1
+            quality += 1
         }
-    }
-
-    override fun update3() {
         if (sellIn < 0) {
             if (quality < 50) {
-                quality = quality + 1
+                quality += 1
             }
         }
     }
 }
 
 class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun update1() {
+    override fun degrade() {
         if (quality < 50) {
-            quality = quality + 1
-            if (sellIn < 11) {
+            quality += 1
+            if (sellIn < 10) {
                 if (quality < 50) {
-                    quality = quality + 1
+                    quality += 1
                 }
             }
-            if (sellIn < 6) {
+            if (sellIn < 5) {
                 if (quality < 50) {
-                    quality = quality + 1
+                    quality += 1
                 }
             }
         }
-    }
-
-    override fun update3() {
         if (sellIn < 0) {
-            quality = quality - quality
+            quality = 0
         }
     }
 }
 
 class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun update1() {}
+    override fun degrade() {}
     override fun age() {}
-    override fun update3() {}
 }
